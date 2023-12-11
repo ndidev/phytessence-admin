77e5e0e6153b30fb73fd0155b31c3f3bdcf814fd
Nicolas DENIS <contact@ndi.dev>
2023-12-11 11:15:55 +0100

    Fix mysql config

--- src/lib/server/src/mysql.ts
+++ src/lib/server/src/mysql.ts
@@ -9,24 +9,26 @@ import {
   MYSQL_BASE,
 } from "$env/static/private";
 
-const connectionUri = {
-  uri: DATABASE_URL,
-  dateStrings: true,
-} satisfies mysql.ConnectionOptions;
+const connectionUri = DATABASE_URL
+  ? {
+      uri: DATABASE_URL,
+    }
+  : ({
+      host: MYSQL_HOST,
+      port: parseInt(MYSQL_PORT),
+      user: MYSQL_USER,
+      password: MYSQL_PASS,
+      database: MYSQL_BASE,
+    } satisfies mysql.ConnectionOptions);
 
-const connectionOptions = {
-  host: MYSQL_HOST,
-  port: parseInt(MYSQL_PORT),
-  user: MYSQL_USER,
-  password: MYSQL_PASS,
-  database: MYSQL_BASE,
+const options = {
   dateStrings: true,
   namedPlaceholders: true,
 } satisfies mysql.ConnectionOptions;
 
-const connection = DATABASE_URL
-  ? await mysql.createConnection(connectionUri)
-  : await mysql.createConnection(connectionOptions);
+const config = { ...connectionUri, ...options };
+
+const connection = await mysql.createConnection(config);
 
 connection.on("error", (err) => {
   console.log("Erreur");
